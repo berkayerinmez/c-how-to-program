78354eb44604448b6e7307402a8ab43bbe777364
Berkay Erinmez <104937892+berkayerinmez@users.noreply.github.com>
2023-10-05 22:11:19 +0300

    Create ex_2.5.c

---
+++ Chapter_2/ex_2.5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z, result;
+    
+    printf("Enter 3 number:\n");
+    
+    scanf("%d%d%d", &x, &y, &z);
+    
+    result = x + y + z;
+    
+    printf("The product is: %d\n", result);
+    
+    return 0;
+}
