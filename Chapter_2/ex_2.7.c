95e3b06517e761f406a8c76537f4c79b9b426ade
Berkay Erinmez <104937892+berkayerinmez@users.noreply.github.com>
2023-10-05 22:27:14 +0300

    Create ex_2.7.c

---
+++ Chapter_2/ex_2.7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    /*a */ scanf( "d", value ); /* the correct way is %d */
+    
+    /* b */ printf( "The product of %d and %d is %d"\n, x, y ); /* \n must be inside of "" */
+    
+    /* c */ firstNumber + secondNumber = sumOfNumbers /* semi column is not written */
+    
+    /* d */ if ( number => largest )
+        largest == number; /* where is { } ? */
+    
+    /* e */ /* comment identificator is the oppiside way */
+    
+    /* f */ Scanf( "%d", anInteger ); /* it must be &anInteger */
+    
+    /* g */ printf( "Remainder of %d divided by %d is\n", x, y, x % y ); /* you cant write it like that dividion need to
+     be another variable and also the third %d is missing */
+    
+    /* h */ if ( x = y );
+    printf( %d is equal to %d\n", x, y ); /*  missing "" */
+           
+    /* i */ print( "The sum is %d\n," x + y ); " /* there should be only one variable */
+    
+    /* j */ Printf( "The value you entered is: %d\n, &value ); " /* missing  and also P is capital */
+    
+}
