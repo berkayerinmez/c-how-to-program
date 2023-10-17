83d1a05f782f1e6a686378ddc18f822f78a2b6da
Berkay Erinmez <104937892+berkayerinmez@users.noreply.github.com>
2023-10-17 20:42:17 +0300

    Update 2.16.c

--- Chapter_2/2.16.c
+++ Chapter_2/2.16.c
@@ -6,7 +6,7 @@ int main(){
     printf("Enter two number: ");
     scanf("%d %d", &num1, &num2);
     
-    printf("sum is: %d \n", num1 + num2);
+    printf("sum is %d \n", num1 + num2);
     printf("product is %d \n", num1 * num2);
     printf("difference is %d \n", num1 - num2);
     printf("quotient is %d \n", num1 / num2);
