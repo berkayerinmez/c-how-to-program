46581400bad0fed4d7513b7c8a67a51a30a7ba66
Berkay Erinmez <104937892+berkayerinmez@users.noreply.github.com>
2023-10-17 20:38:47 +0300

    Update 2.16.c

--- Chapter_2/2.16.c
+++ Chapter_2/2.16.c
@@ -1,37 +1,15 @@
 #include <stdio.h>
 
-void sum (int num1, int num2){
-    
-    printf("sum is %d\n", num1 + num2);
-}
-
-void product (int num1, int num2){
-    
-    printf("product is %d\n", num1 * num2);
-    
-}
-
-void difference (int num1, int num2){
-    printf("difference is %d\n", num1 - num2);
-}
-
-void quotient(int num1, int num2){
-    printf("quotient is %d\n", num1 / num2);
-}
-
-void remain ( int num1, int num2){
-    printf("remainder is %d\n", num1 % num2);
-}
-
-int num1, num2;
-
 int main(){
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-    sum(num1,num2);
-    product(num1, num2);
-    quotient(num1, num2);
-    remain(num1, num2);
+    int num1, num2;
     
-    return 0;
+    printf("Enter two number: ");
+    scanf("%d %d", &num1, &num2);
+    
+    printf("sum is: %d \n", num1 + num2);
+    printf("product is %d \n", num1 * num2);
+    printf("difference is %d \n", num1 - num2);
+    printf("quotient is %d \n", num1 / num2);
+    printf("remainder is %d \n", num1 & num2);
+    
 }
