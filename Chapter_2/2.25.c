32f95d4480884d2388d27409f69fc040447da0e7
Berkay Erinmez <104937892+berkayerinmez@users.noreply.github.com>
2023-10-18 23:19:23 +0300

    Create 2.25.c

---
+++ Chapter_2/2.25.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("BBBBBBBBBBBBBBBBB\n");
+    printf("B       B       B\n");
+    printf("B       B       B\n");
+    printf("B       B       B\n");
+    printf(" B     B B     B\n");
+    printf("  BBBBB   BBBBB\n\n");
+    //printf("   BBB     BBB\n\n");
+    
+    printf("EEEEEEEEEEEEEEEEE\n");
+    printf("E       E       E\n");
+    printf("E       E       E\n");
+    printf("E       E       E\n");
+    printf("E       E       E\n\n");
+    //printf("E       E       E\n");
+    //printf("E       E       E\n");
+  
+    return 0;
+}
