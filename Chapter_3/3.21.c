fd37425b320360f41b28e1fadb12ff3d32ba86a4
Berkay Erinmez <104937892+berkayerinmez@users.noreply.github.com>
2023-11-04 23:07:31 +0300

    Salary Calculator

---
+++ Chapter_3/3.21.c
@@ -0,0 +1,28 @@
+void salary(float hours, float rate){
+    float extra_Hours;
+    if (hours <= 40)
+        printf("Salary is $%.2f\n", hours * rate);
+    else {
+        extra_Hours = hours - 40;
+        hours = 40;
+        printf("Salary is $%.2f\n", hours * rate + extra_Hours * 1.5 * rate);
+    }
+}
+
+int main(){
+    
+    float hours_Worked, hourly_Rate;
+    
+    while(hours_Worked != -1){
+        printf("Enter # of hours worked (-1 to end): ");
+        scanf("%f", &hours_Worked);
+        if(hours_Worked == -1)
+            break;
+        printf("Enter hourly rate of the worker ($00.00): ");
+        scanf("%f", &hourly_Rate);
+        
+        salary(hours_Worked, hourly_Rate);
+    }
+    
+    return 0;
+}
