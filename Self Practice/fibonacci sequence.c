05d5f269288139a02e1595e979a35e71f8965800
Berkay Erinmez <104937892+berkayerinmez@users.noreply.github.com>
2023-12-27 13:11:58 +0300

    Create fibonacci sequence.c

---
+++ Self Practice/fibonacci sequence.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double fibon( int b){
+    double f2= 1;
+    double f1= 0;
+    double i = 2;
+    double new = 0;
+    if(b == 0)
+        return 0;
+    else if (b == 1)
+        return 1;
+    while (i<=b){
+        new = f1 + f2;
+        i++;
+        f1= f2;
+        f2 = new;
+    }
+    return new;
+    
+}
+
+int main(){
+    printf( "%0.f\n", fibon(19));   
+}
